12200db0b653d84f692808e00330f8fb5986b1a2
tjdgus333 <skyworld321@naver.com>
2026-07-05 20:35:42 -0500

    Reordered modifiers to comply with the Java Language Specification

--- src/main/java/smells/BadCodeExample2.java
+++ src/main/java/smells/BadCodeExample2.java
@@ -3,7 +3,7 @@ package smells;
 public class BadCodeExample2 {
 
     // Reorder Modifier Target
-    final static public int MULTIPLIER = 10;
+    public static final int MULTIPLIER = 10;
 
     // Unused Private Field Target
     private String unusedField = "unused";
